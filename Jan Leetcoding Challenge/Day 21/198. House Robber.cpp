124a59211d6099f5846583a1f72229929e1a105a
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-01-22 01:09:16 +0530

    Update 198. House Robber.cpp

--- Jan Leetcoding Challenge/Day 21/198. House Robber.cpp
+++ Jan Leetcoding Challenge/Day 21/198. House Robber.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
 
+    int f(int ind,vector<int>&nums,vector<int>&dp){
+
+        if(ind>=nums.size()) return 0;
+
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+
+        int take = nums[ind]+f(ind+2,nums,dp);
+        int notTake = f(ind+1,nums,dp);
+
+        return dp[ind]= max(take,notTake);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        return f(0,nums,dp);
+    }
+};
