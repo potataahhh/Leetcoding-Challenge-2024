c622e51517b78c99a72b8fac9fd7d59880675723
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-01-29 01:08:37 +0530

    Update 1074. Number of Submatrices That Sum to Target.cpp

--- Jan Leetcoding Challenge/Day 28/1074. Number of Submatrices That Sum to Target.cpp
+++ Jan Leetcoding Challenge/Day 28/1074. Number of Submatrices That Sum to Target.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    int getCount(vector<int> &prefix, int target){
+        unordered_map<int,int> m;
+        int sum = 0;
+        int res = 0;
+        m[0]=1;
+        for(auto i : prefix){
+            sum += i;
+            if(m[sum-target] > 0){
+                res += m[sum-target];
+            }
+            m[sum]++;
+        }
+        return res;
+    }
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int res = 0;
+        
+        for(int i=0;i<m; i++){
+            vector<int> prefix(n, 0);
+            for(int j=i; j<m; j++){
+                for(int k=0;k<n;k++){
+                    prefix[k] += matrix[j][k];
+                }
+                res += getCount(prefix, target);
+            }
+        }
+        return res;
+    }
+};
